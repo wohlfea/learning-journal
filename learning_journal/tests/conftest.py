10496c03a788f515854302864f4ec6ed3bd02c53
paulsheridan <paul.sheridan@me.com>
2016-03-15 18:17:12 -0700

    changed file structure

---
+++ learning_journal/tests/conftest.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import pytest
+from sqlalchemy import create_engine
+
+from learning_journal.models import DBSession, Base
+
+
+TEST_DATABASE_URL = 'postgresql://paulsheridan:@localhost:5432/learning-journal'
+
+
+@pytest.fixture(scope='session')
+def sqlengine(request):
+    engine = create_engine(TEST_DATABASE_URL)
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+
+    def teardown():
+        Base.metadata.drop_all(engine)
+
+    request.addfinalizer(teardown)
+    return engine
+
+
+@pytest.fixture()
+def dbtransaction(request, sqlengine):
+    connection = sqlengine.connect()
+    transaction = connection.begin()
+    DBSession.configure(bind=connection)
+
+    def teardown():
+        transaction.rollback()
+        connection.close()
+        DBSession.remove()
+
+    request.addfinalizer(teardown)
+
+    return connection
