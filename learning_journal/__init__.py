204d1ec7df4569b1ad73af6ab4f2e94f25487f60
paulsheridan <paul.sheridan@me.com>
2016-03-16 18:13:11 -0700

    added base and list jinja templates

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -24,5 +24,6 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('my_route', '/entries')
     config.scan()
     return config.make_wsgi_app()
