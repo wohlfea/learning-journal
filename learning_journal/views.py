a3efc8791df794e6faa0ddbabd9606e8667236a2
paulsheridan <paul.sheridan@me.com>
2016-03-17 18:53:03 -0700

    /add routing working, views updated

--- learning_journal/views.py
+++ learning_journal/views.py
@@ -1,5 +1,7 @@
 from pyramid.view import view_config
-from .models import (Entry, DBSession)
+from .models import Entry, DBSession
+from wtforms import Form, StringField, TextAreaField, validators
+from pyramid.httpexceptions import HTTPFound
 
 
 @view_config(route_name='index_route', renderer='templates/list.jinja2')
@@ -13,3 +15,30 @@ def entry_view(request):
     entry_id = '{entry}'.format(**request.matchdict)
     entry_data = DBSession.query(Entry).filter(Entry.id == entry_id).first()
     return {'entry': entry_data}
+
+
+@view_config(route_name='new_route', renderer='templates/add.jinja2')
+def add_view(request):
+    form = get_form_entry(request)
+    return {'form': form}
+
+
+# @view_config(route_name='edit_route', renderer='templates/edit.jinja2')
+# def edit_view(request):
+#     entry_id = request.matchdict['entry']
+
+
+class EntryForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=128)])
+    text = TextAreaField('Content', [validators.InputRequired()])
+
+
+def get_form_entry(request):
+    form = EntryForm(request.POST)
+    if request.method == 'POST' and form.validate():
+        entry = Entry()
+        entry.title = form.title
+        entry.text = form.text
+        DBSession.add(entry)
+        return HTTPFound(location="/entry/{id}")
+    return {'form': form, 'action': request.matchdict.get('action')}
