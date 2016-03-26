752552f0a3f13bbbd55d26152382f4085d36772e
paulsheridan <paul.sheridan@me.com>
2016-03-26 14:47:31 -0700

    security updated

--- learning_journal/security.py
+++ learning_journal/security.py
@@ -4,6 +4,4 @@ import os
 
 def check_password(password):
     hashed_password = os.environ.get('AUTH_PASSWORD', 'password failure')
-    # hash = pwd_context.encrypt(password)
-    # print('hash:', hash)
     return pwd_context.verify(password, hashed_password)
